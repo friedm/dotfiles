957cb9533d604a0e211c45695ee54d2a285b16c0
Matt Friedman <matt@friedm.com>
2017-09-14 00:31:06 -0700

    add bashrc ssh-agent config

--- setup.sh
+++ setup.sh
@@ -1,12 +1,14 @@
+set -e
+
 git submodule update --init
 stow bash --no-folding
+cp ./dotfiles.env.default $HOME/.dotfiles.env
 stow fish --no-folding
 stow user-dirs --no-folding
 stow git --no-folding
-ln -s $(pwd)/git/.gitignore ../.gitignore
 stow tmux --no-folding
+mkdir ~/.yr &> /dev/null || true
 stow vim --no-folding
 stow nvim --no-folding
 vim +PlugInstall +qall
 nvim +PlugInstall +qall
-
