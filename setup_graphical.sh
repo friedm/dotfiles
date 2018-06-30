6bd8e4cb2d7c6e709b37bd3d55b9c64dfdb4a72b
Matt Friedman <matt@friedm.com>
2018-06-30 09:54:10 -0700

    use systemd service to lock with physlock on sleep

--- setup_graphical.sh
+++ setup_graphical.sh
@@ -5,3 +5,7 @@ stow urxvt --no-folding
 stow rofi --no-folding
 stow vimperator --no-folding
 stow tridactyl --no-folding
+
+cat lock.service | envsubst | sudo tee /etc/systemd/system/lock.service > /dev/null
+sudo systemctl enable lock.service
+
