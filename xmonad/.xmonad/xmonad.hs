33122d56e49a0b914180ef69fe1c1ca926c9ccfe
Matt Friedman <matt@friedm.com>
2018-07-11 09:37:58 -0700

    use z to fullscreen

--- xmonad/.xmonad/xmonad.hs
+++ xmonad/.xmonad/xmonad.hs
@@ -6,6 +6,8 @@ import XMonad.Layout.ToggleLayouts
 import XMonad.Layout.NoBorders
 import XMonad.Layout.Fullscreen
 import XMonad.Layout.ResizableTile ( ResizableTall(..), MirrorResize(MirrorShrink, MirrorExpand) )
+import XMonad.Layout.MultiToggle ( single, mkToggle, Toggle(..) )
+import XMonad.Layout.MultiToggle.Instances ( StdTransformers(..) )
 import XMonad.Hooks.ManageDocks (avoidStruts)
 import System.IO
 import Data.Monoid
@@ -18,19 +20,19 @@ import qualified Data.Map        as M
 myKeys =
   [ ("M-<Return>", spawn "rofi -show window")
   , ("M-S-<Return>", spawn "rofi -show run")
-  , ("M-f", (sendMessage (Toggle "Full")))
   , ("M-b", spawn "urxvt -e /bin/bash")
   , ("M-t", spawn "urxvt -e /bin/bash -c 'tmux attach || tmux'")
   , ("M-S-t", spawn "urxvt -e /bin/zsh")
   , ("M-S-l", spawn "xautolock -locknow")
   , ("M-S-<Space>", withFocused $ windows . W.sink)
+  , ("M-z", sendMessage $ XMonad.Layout.MultiToggle.Toggle FULL)
   , ("M-<Up>", (sendMessage MirrorExpand))
   , ("M-<Left>", (sendMessage MirrorExpand))
   , ("M-<Down>", (sendMessage MirrorShrink))
   , ("M-<Right>", (sendMessage MirrorShrink))
   ]
 
-myLayout = tall ||| Mirror tall ||| Full
+myLayout = mkToggle (single FULL) (tall ||| Mirror tall)
   where tall = ResizableTall 1 (3/100) (1/2) []
 
 main = do xmonad $ ewmh $ fullscreenSupport $ defaultConfig {
